65fda557bc1846c7735c637705aa5a10cdea35d1
JoHyerin <gpfls468@gmail.com>
2024-11-23 12:51:01 +0900

    [Add] #12 - 프로젝트 생성

---
+++ 35th-SOPT-Seminar-SwiftUI/35th-SOPT-Seminar-SwiftUI/_5th_SOPT_Seminar_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  _5th_SOPT_Seminar_SwiftUIApp.swift
+//  35th-SOPT-Seminar-SwiftUI
+//
+//  Created by 조혜린 on 11/23/24.
+//
+
+import SwiftUI
+
+@main
+struct _5th_SOPT_Seminar_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
